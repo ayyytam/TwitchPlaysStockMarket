f7ddf66afb4b30284966862cb9e1a408c5d2214e
Robert Li <li.robertj+git@gmail.com>
2015-04-18 12:24:46 -0400

    Serve static pages from Angular folder

--- node/app.js
+++ node/app.js
@@ -22,7 +22,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Angular')));
+
+// share an object of the data
+var data = {};
+app.use(function (request, response, next) {
+    request.data = data;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
