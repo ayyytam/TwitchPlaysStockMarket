3ddedac5dbb0cf21739d8e70f7a41b9ae201990f
Andrew Tam <andrew.tam.sp@gmail.com>
2015-04-18 11:55:47 -0400

    add a bunch of stuff

--- Angular/app.js
+++ Angular/app.js
@@ -1,10 +1,10 @@
 var twitchPlaysStockMarket = angular.module('twitchPlaysStockMarket', []);
 
-twitchPlaysStockMarket.controller('StockController', function($scope, $log, $filter) {
+twitchPlaysStockMarket.controller('DashboardController', function($scope, $log, $filter) {
 	$scope.message = 'twitch plays stock market';
+	$scope.stocks = {GOOG: "Google", AAPL: "Apple", BLAH: "Blah"};
+	//$scope.stocks = ["asdfa", "asdfasdf", "adfasdfads"];
 	$log.info($scope);
 	$log.info($scope.message);
 	console.log("helllloo");
-});
-
-console.log("test");
+});	
