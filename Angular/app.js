7da9f3b02a260b09b364bf28156efdd10c0209eb
Andrew Tam <andrew.tam.sp@gmail.com>
2015-04-18 18:36:56 -0400

    Add parallax scrolling with background image

--- Angular/app.js
+++ Angular/app.js
@@ -220,3 +220,13 @@ twitchPlaysStockMarket.filter('gainLossFilter', ['$filter', function ($filter) {
 	};
 }]);
 
+// Parallax scrolling
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
