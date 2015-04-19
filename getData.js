fd2ea6f08223c565384c73b1a618b18d502a89ba
Gabe Fernando <gef756@gmail.com>
2015-04-19 11:11:29 -0400

    FIX: increase data pull to 3 hours (#19 temp fix)

--- getData.js
+++ getData.js
@@ -27,14 +27,14 @@ function getLastN(ticker, n) {
 function getNewStock(ticker) {
     tickData[ticker] = [];
 
-    var url = 'http://globalquotes.xignite.com/v3/xGlobalQuotes.json/GetBars?IdentifierType=Symbol&Identifier=' + ticker + '&StartTime=4/16/2015%209:30%20AM&EndTime=4/16/2015%2010:30%20am&Precision=Seconds&Period=1&_token=6E10075F14A6447E94C8700F8CF7116A';
+    var url = 'http://globalquotes.xignite.com/v3/xGlobalQuotes.json/GetBars?IdentifierType=Symbol&Identifier=' + ticker + '&StartTime=4/16/2015%209:30%20AM&EndTime=4/16/2015%2012:30%20pm&Precision=Seconds&Period=1&_token=6E10075F14A6447E94C8700F8CF7116A';
     var response = request('GET', url);
     var body = JSON.parse(response.getBody());
 
     if (!body.Bars || body.Bars.length < 200) {
         return false;
     }
-    
+
     body.Bars.forEach(function(bar) {
         tickData[ticker].push(bar.Close);
     });
